bef9667ef989a5674ba6bf16ad25d0f19c552637
Med Elboudali <med.sneaky@gmail.com>
2020-02-28 12:39:41 +0000

    Added JWT and get JWT token with user id

--- models/routes/Users.js
+++ models/routes/Users.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const router = express.Router();
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const User = require("../Schema/User");
+const router = express.Router();
 
 //init midlleware
 router.use(express.json({ extended: false }));
@@ -24,22 +26,39 @@ router.post(
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
+    //Express Validator
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    //Mongoose Schema
     const { name, email, password } = req.body;
-
     try {
-      let user = await User.findOne({email});
+      //Search for the email in mongodb
+      let user = await User.findOne({ email });
       if (user) {
+        //if we found the email
         return res.status(400).json({ Msg: "User already exist!" });
       }
+      //if we didnt found the email we will added new user
       user = new User({ name, email, password });
+      //Bcrypt and hash password
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+      //Added the user to our DB
       await user.save();
-      res.send("User Saved");
+      //Create JWT token to sign in
+      const payload = { user: { id: user.id } };
+      
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({token});
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
