e5cdbb334b99568c3dbad0b68b549ea988006a45
Med Elboudali <med.sneaky@gmail.com>
2020-03-28 23:06:49 +0000

    debug and deploy

--- Server.js
+++ Server.js
@@ -6,9 +6,9 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/users", require("./models/Routes/Users"));
-app.use("/api/auth", require("./models/Routes/Auth"));
-app.use("/api/contacts", require("./models/Routes/Contacts"));
+// app.use("/api/users", require("./models/Routes/Users"));
+// app.use("/api/auth", require("./models/Routes/Auth"));
+// app.use("/api/contacts", require("./models/Routes/Contacts"));
 
 //connect database
 connectDB();
