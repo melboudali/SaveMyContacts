b21d065588b6cce334e3b345674320f512bea23a
Med Elboudali <med.sneaky@gmail.com>
2020-03-28 22:34:20 +0000

    ignoring build file

--- Server.js
+++ Server.js
@@ -14,9 +14,11 @@ app.use("/api/contacts", require("./models/Routes/Contacts"));
 connectDB();
 
 // Serve Static assets in production
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+}
 
 app.listen(PORT, () => console.log("Server Conected"));
