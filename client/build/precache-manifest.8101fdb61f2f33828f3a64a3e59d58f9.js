9e1d41ba0e7e5e43dcba6574c4574eda219f73c9
Med Elboudali <med.sneaky@gmail.com>
2020-03-28 22:00:05 +0000

    run build for production and deploy to netlify

---
+++ client/build/precache-manifest.8101fdb61f2f33828f3a64a3e59d58f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2060c466b4b509ee2798ab0162928434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02284c8e0a2ac3b6e6e9",
+    "url": "/static/css/main.35b9d75c.chunk.css"
+  },
+  {
+    "revision": "7579bfe1e4240830ac07",
+    "url": "/static/js/2.8a8d84ad.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.8a8d84ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02284c8e0a2ac3b6e6e9",
+    "url": "/static/js/main.cca23087.chunk.js"
+  },
+  {
+    "revision": "6c892f14a339b9737394",
+    "url": "/static/js/runtime-main.0b577346.js"
+  }
+]);
