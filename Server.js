782835604c0ee25cddc936cce0a11a34bd9ad728
Med Elboudali <med.sneaky@gmail.com>
2020-03-31 21:16:27 +0000

    :white_check_mark: MERN App: Ready, JWT exp: 24H

--- Server.js
+++ Server.js
@@ -16,9 +16,9 @@ app.use("/api/contacts", require("./routes/contacts"));
 // Serve Static HTML in production "index.html"
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", res => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 app.listen(PORT);
